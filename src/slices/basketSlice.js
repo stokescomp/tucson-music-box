8239dc6e471b7a677a827f64cd8eb5862ad21014
Kevin <kevin.castro.chavez0@gmail.com>
2022-10-07 16:42:40 -0600

    first commit

---
+++ src/slices/basketSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  items: [],
+};
+
+export const basketSlice = createSlice({
+  name: 'basket',
+  initialState,
+  reducers: {
+    addToBasket: (state, action) => {},
+    removeFromBasket: (state, action) => {},
+  },
+});
+
+export const { addToBasket, removeFromBasket } = basketSlice.actions;
+
+export default basketSlice.reducer;
