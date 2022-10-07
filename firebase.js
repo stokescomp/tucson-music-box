d9f375be9348278dc7dc921bc37b086a92a42abe
Kevin <kevin.castro.chavez0@gmail.com>
2022-10-07 17:07:08 -0600

    remove from gitignore

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDwJr_5c_Ubxw-i7onPNXh5G-lfVx9Op_U',
+  authDomain: 'tucson-music-box-57dd0.firebaseapp.com',
+  projectId: 'tucson-music-box-57dd0',
+  storageBucket: 'tucson-music-box-57dd0.appspot.com',
+  messagingSenderId: '815477013504',
+  appId: '1:815477013504:web:ec7be752ff79a066282ce5',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
