103bfa03f64cc2cf1c112b3635ff8051404274da
Matthew James <matthewj1561@gmail.com>
2022-10-07 20:06:42 -0600

    Working hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import Index from "../components/categories-carousel";
+import BasicSlider from "../components/hero";
 
 export default function Home() {
   return (
     <div>
+      <BasicSlider />
       <Index />
     </div>
   );
