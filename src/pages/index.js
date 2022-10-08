879efaaa79022d88e07d11c354f819377f6e2b99
Jacksonckc <jackson858216047@gmail.com>
2022-10-07 20:30:11 -0600

    yo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,34 @@
-import Index from "../components/categories-carousel";
+import Index from '../components/categories-carousel';
 
-import BasicSlider from "../components/hero";
+import BasicSlider from '../components/hero';
 
-import MyCarousel from "../components/accesories/index.js"
-
-
+import MyCarousel from '../components/accesories/index.js';
 
 export default function Home() {
   return (
     <div>
-
       <BasicSlider />
       <Index />
 
       <div>
-        <h1 className={index.title}>Categories</h1>
+        <h1>Categories</h1>
         <span>Rediscover your childhood</span>
         <span>Book Viking Treasures</span>
         <button>Shop All</button>
       </div>
       <Index />
+      <div>
+        <h1>Accesories</h1>
         <div>
-          <h1 className={index.title}>Accesories</h1>
-          <div>
-            <p>Gems, minerals, rose quartz, jewelry, 
-            and butterfly specimens fill our storefront. 
-            Now you can take a peek at our collection from 
-            the comfort of your own home. Each different piece 
-            would make a great new home accent or gift for any 
-            loved one!</p>
-          </div>
+          <p>
+            Gems, minerals, rose quartz, jewelry, and butterfly specimens fill
+            our storefront. Now you can take a peek at our collection from the
+            comfort of your own home. Each different piece would make a great
+            new home accent or gift for any loved one!
+          </p>
         </div>
+      </div>
       <MyCarousel />
-
-
     </div>
   );
 }
