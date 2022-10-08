36f4562e76a436903cdb2dbb97b530d8c4c4a981
Jacksonckc <jackson858216047@gmail.com>
2022-10-07 19:26:53 -0600

    authentication and is loggedin state

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
+import { Provider } from 'react-redux';
+import { store } from '../app/store';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <div>
+      <Link href='/login'>Profile</Link>
       <h1>Hello</h1>
     </div>
   );
