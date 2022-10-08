36785f8c21049dd048fea0900e26a4c186faa0d2
Kevin <kevin.castro.chavez0@gmail.com>
2022-10-08 09:33:30 -0600

    yo

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import style from "./comps.module.scss";
+import style from './comps.module.scss';
 import React from 'react';
 import { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
@@ -9,21 +9,20 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { Button } from '@mui/material';
 import { useSelector } from 'react-redux';
-import { fetchUserInfo } from "../slices/userSlice";
+import { fetchUserInfo } from '../slices/userSlice';
 import AdbIcon from '@mui/icons-material/Adb';
 import DiamondIcon from '@mui/icons-material/Diamond';
 import { useRouter } from 'next/router';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import Badge from '@mui/material/Badge';
 
-import { fetchUserInfo } from '../slices/userSlice';
 import { selectProducts } from '../slices/basketSlice';
 
 let pages;
-const productsLink = <Link href="/shop">Products</Link>;
+const productsLink = <Link href='/shop'>Products</Link>;
 
 const pagesLoggedOut = [productsLink];
-const pagesLoggedIn = [productsLink, "My Orders"];
+const pagesLoggedIn = [productsLink, 'My Orders'];
 
 export default function ButtonAppBar() {
   const router = useRouter();
@@ -61,55 +60,56 @@ export default function ButtonAppBar() {
   return (
     <div>
       <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
+        <AppBar position='static'>
           <Toolbar className={style.header}>
             <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
+              size='large'
+              edge='start'
+              color='inherit'
+              aria-label='menu'
               sx={{ mr: 2 }}
             ></IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              <Link href="/">
-                <Button sx={{ my: 2, color: "white", display: "block" }}>
+            <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+              <Link href='/'>
+                <Button sx={{ my: 2, color: 'white', display: 'block' }}>
                   <DiamondIcon />
                 </Button>
               </Link>
-              <Link href="/contact">
-                <Button sx={{ my: 2, color: "white", display: "block" }}>
+              <Link href='/contact'>
+                <Button sx={{ my: 2, color: 'white', display: 'block' }}>
                   Contact
                 </Button>
               </Link>
             </Typography>
 
-            <Box sx={{ flexGrow: 1, display: { md: "flex" } }}>
+            <Box sx={{ flexGrow: 1, display: { md: 'flex' } }}>
               {pages.map((page) => (
                 <Button
                   key={page}
                   onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: "white", display: "block" }}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
                 >
                   {page}
                 </Button>
               ))}
             </Box>
 
-          <Link href='/profile'>
-            <Button color='inherit'>Login</Button>
-          </Link>
+            <Link href='/profile'>
+              <Button color='inherit'>Login</Button>
+            </Link>
 
-          <Link href='/cart'>
-            <Badge
-              badgeContent={products.length}
-              showZero={true}
-              color='secondary'
-            >
-              <ShoppingCartIcon />
-            </Badge>
-          </Link>
-        </Toolbar>
-      </AppBar>
-    </Box>
+            <Link href='/cart'>
+              <Badge
+                badgeContent={products.length}
+                showZero={true}
+                color='secondary'
+              >
+                <ShoppingCartIcon />
+              </Badge>
+            </Link>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </div>
   );
 }
