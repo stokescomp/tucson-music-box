593dbb6e25a26b6eab47cf4377fc714788a08ae6
Kevin <kevin.castro.chavez0@gmail.com>
2022-10-08 03:21:27 -0600

    added link for the product to go to dynamic url

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import React from 'react';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
+import Link from 'next/link';
 
 import styles from './styles.module.css';
 
@@ -16,16 +17,18 @@ function Product({
   quantity,
 }) {
   return (
-    <div className={styles.product}>
-      <img src={imgUrl} alt={name} />
+    <Link href={`shop/${name}`}>
+      <div className={styles.product}>
+        <img src={imgUrl} alt={name} />
 
-      <div className={styles.product__info}>
-        <h4 className={styles.product__name}>{name}</h4>
-        <span>${price}</span>
-        <EditIcon />
-        <DeleteIcon />
+        <div className={styles.product__info}>
+          <h4 className={styles.product__name}>{name}</h4>
+          <span>${price}</span>
+          <EditIcon />
+          <DeleteIcon />
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
