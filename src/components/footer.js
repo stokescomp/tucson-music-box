3b4e7d37156e48de76202c6e0d4c83ea3f8288e6
mlutz06 <madison.lutz@sentinelofsafety.com>
2022-10-08 00:05:20 -0600

    contact page

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,10 @@
 import * as React from "react";
-
 export default function Footer() {
     return (
       <>
+      <div>
         <span>Copyright sitename.com All Rights Reserved</span>
+        </div>
       </>
     );
   }
