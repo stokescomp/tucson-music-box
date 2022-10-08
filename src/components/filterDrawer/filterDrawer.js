aa01bda4078b708ef09903d4fc628eaf0b100b14
Matthew James <matthewj1561@gmail.com>
2022-10-08 13:53:15 -0600

    filter progress

--- src/components/filterDrawer/filterDrawer.js
+++ src/components/filterDrawer/filterDrawer.js
@@ -1,31 +1,32 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import { Global } from '@emotion/react';
-import { styled } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { grey } from '@mui/material/colors';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import Skeleton from '@mui/material/Skeleton';
-import Typography from '@mui/material/Typography';
-import SwipeableDrawer from '@mui/material/SwipeableDrawer';
-import Checkbox from '@mui/material/Checkbox';
+import * as React from "react";
+import PropTypes from "prop-types";
+import { Global } from "@emotion/react";
+import { styled } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { grey } from "@mui/material/colors";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Skeleton from "@mui/material/Skeleton";
+import Typography from "@mui/material/Typography";
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
+import Checkbox from "@mui/material/Checkbox";
 
-import styles from './filterDrawer.module.css';
-import { FormControlLabel, FormGroup } from '@mui/material';
+import styles from "./filterDrawer.module.css";
+import { FormControlLabel, FormGroup } from "@mui/material";
+import { handleClientScriptLoad } from "next/script";
 
 const drawerBleeding = -50;
 
-const Root = styled('div')(({ theme }) => ({
-  height: '100%',
+const Root = styled("div")(({ theme }) => ({
+  height: "100%",
   backgroundColor:
-    theme.palette.mode === 'light'
+    theme.palette.mode === "light"
       ? grey[100]
       : theme.palette.background.default,
 }));
 
 const StyledBox = styled(Box)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'light' ? '#354ace' : grey[800],
+  backgroundColor: theme.palette.mode === "light" ? "#354ace" : grey[800],
 }));
 
 // const Puller = styled(Box)(({ theme }) => ({
@@ -41,7 +42,26 @@ const StyledBox = styled(Box)(({ theme }) => ({
 function FilterDrawer(props) {
   const { window } = props;
   const [open, setOpen] = React.useState(false);
+  const [checkboxesState, setCheckboxesState] = React.useState({
+    gems: false,
+    fossils: false,
+    lamps: false,
+    keepsake: false,
+    accesories: false,
+    misc: false,
+  });
 
+  const handleCheckChange = (e) => {
+    setCheckboxesState({
+      ...checkboxesState,
+      [e.target.name]: e.target.checked,
+    });
+  };
+
+  React.useEffect(() => {
+    // Update the document title using the browser API
+    props.filter(checkboxesState);
+  });
   const toggleDrawer = (newOpen) => () => {
     setOpen(newOpen);
   };
@@ -49,13 +69,14 @@ function FilterDrawer(props) {
   // This is used only for the example
   const container =
     window !== undefined ? () => window().document.body : undefined;
+  const { gems, fossils, lamps, keepsake, accessories, misc } = checkboxesState;
 
   return (
     <Root className={styles.filterDrawer__toggler}>
       <Button onClick={toggleDrawer(true)}>Categories</Button>
       <SwipeableDrawer
         container={container}
-        anchor='bottom'
+        anchor="bottom"
         open={open}
         onClose={toggleDrawer(false)}
         onOpen={toggleDrawer(true)}
@@ -67,16 +88,16 @@ function FilterDrawer(props) {
       >
         <StyledBox
           sx={{
-            position: 'absolute',
+            position: "absolute",
             top: 0,
             borderTopLeftRadius: 8,
             borderTopRightRadius: 8,
-            visibility: 'visible',
+            visibility: "visible",
             right: 0,
             left: 0,
           }}
         >
-          <Typography sx={{ p: 2, color: 'text.secondary' }}>
+          <Typography sx={{ p: 2, color: "text.secondary" }}>
             Filters
           </Typography>
         </StyledBox>
@@ -84,17 +105,71 @@ function FilterDrawer(props) {
           sx={{
             px: 2,
             pb: 2,
-            height: '100%',
-            overflow: 'auto',
+            height: "100%",
+            overflow: "auto",
           }}
         >
           <FormGroup className={styles.filterDrawer__filtersContainer}>
-            <FormControlLabel control={<Checkbox />} label='Label' />
-            <FormControlLabel control={<Checkbox />} label='Label' />
-            <FormControlLabel control={<Checkbox />} label='Label' />
-            <FormControlLabel control={<Checkbox />} label='Label' />
-            <FormControlLabel control={<Checkbox />} label='Label' />
-            <FormControlLabel control={<Checkbox />} label='Label' />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={gems}
+                  onChange={handleCheckChange}
+                  name="gems"
+                />
+              }
+              label="Label"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={fossils}
+                  onChange={handleCheckChange}
+                  name="fossils"
+                />
+              }
+              label="Label"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={lamps}
+                  onChange={handleCheckChange}
+                  name="lamps"
+                />
+              }
+              label="Label"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={keepsake}
+                  onChange={handleCheckChange}
+                  name="keepsake"
+                />
+              }
+              label="Label"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={accessories}
+                  onChange={handleCheckChange}
+                  name="accesories"
+                />
+              }
+              label="Label"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={misc}
+                  onChange={handleCheckChange}
+                  name="misc"
+                />
+              }
+              label="Label"
+            />
           </FormGroup>
         </StyledBox>
       </SwipeableDrawer>
