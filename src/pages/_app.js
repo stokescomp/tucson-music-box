35ccca3d0df6a9ecfc5f095fd8b1756d8627589c
Michael Stokes <stokescomp@gmail.com>
2022-12-31 13:38:44 -0700

    add style priority for mui.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,15 @@ import Layout from '../components/layout';
 import '../styles/styles.css';
 import { Provider } from 'react-redux';
 import { store } from '../app/store';
+import { StyledEngineProvider } from '@mui/material/styles';
+
+export function GlobalCssPriority() {
+  return (
+    <StyledEngineProvider injectFirst>
+      {/* Your component tree. Now you can override MUI's styles. */}
+    </StyledEngineProvider>
+  );
+}
 
 function MyApp({ Component, pageProps }) {
   return (
