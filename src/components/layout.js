56027bf7725d40c273f06e6d145cbbbcfd39d72a
Michael Stokes <stokescomp@gmail.com>
2023-02-10 22:05:00 -0700

    add redirect

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,7 @@ import Footer from "./footer";
 export default function Layout({ children }) {
   return (
     <>
+      <meta http-equiv="refresh" content="0; url=https://bookviking.com"></meta>
       <Header />
       <main>{children}</main>
       <Footer />
